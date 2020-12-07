4cc890a2971c67862d31a9c2d8a348ea7b9be1ca
kei-yokoyama <74888936+kei-yokoyama@users.noreply.github.com>
2020-12-07 18:31:40 +0900

    Revert "Revert "既読機能実装""
    This reverts commit cdc6dd48913f2a973facf8329ce4e0b41b055667.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index  # indexアクションを定義した
 
-    @posts = Post.all  # すべてのレコードを@postsに代入
+    @posts = Post.all.order(id: "DESC")  # すべてのレコードを@postsに代入
 
   end
 
@@ -9,6 +9,19 @@ class PostsController < ApplicationController
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
+  end
+
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
   end
 
 end
