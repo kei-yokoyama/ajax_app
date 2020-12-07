75db3ef926f206dab287a5ef93a0920dfd75bac1
kei-yokoyama <74888936+kei-yokoyama@users.noreply.github.com>
2020-12-07 19:07:46 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,8 @@ class PostsController < ApplicationController
 
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
 
 
@@ -25,3 +25,4 @@ class PostsController < ApplicationController
   end
 
 end
+
